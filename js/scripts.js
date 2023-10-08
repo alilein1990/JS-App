8393270641bb41ec57015794ed773ecb60592d65
alilein1990 <alina.leinweber@gmx.de>
2023-10-08 20:04:12 +0200

    added for loop to display pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,10 @@
 pokemonList = [
-    {name: 'Bulbasaur', height: 70, types: [grass, poison]}, 
-    {name: 'Butterfree', height: 110, types: [bug, flying]}, 
-    {name: 'Ariados', height: 110, types: [bug, poison]}, 
-    {name: 'Slowbro', height: 160, types: [psychic, water]}
+    {name: 'Bulbasaur', height: 70, types: ['grass', ' poison']}, 
+    {name: 'Butterfree', height: 110, types: ['bug', ' flying']}, 
+    {name: 'Ariados', height: 110, types: ['bug', ' poison']}, 
+    {name: 'Slowbro', height: 160, types: ['psychic', ' water']}
 ];
+
+for(i = 0; i < pokemonList.length; i++){
+    document.write(`<div>${pokemonList[i].name}: Height: ${pokemonList[i].height} / Types: ${pokemonList[i].types} </div>`);
+}
