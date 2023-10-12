6e55f3a5ae1c87a5d0f8318173727a68cf66936e
alilein1990 <alina.leinweber@gmx.de>
2023-10-12 14:46:03 +0200

    changes to forEach & added IIFE +add/getAll function

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,36 @@
-pokemonList = [
-    { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
-    { name: 'Butterfree', height: 110, types: ['bug', ' flying'] },
-    { name: 'Ariados', height: 110, types: ['bug', ' poison'] },
-    { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
+        { name: 'Butterfree', height: 110, types: ['bug', ' flying'] },
+        { name: 'Ariados', height: 110, types: ['bug', ' poison'] },
+        { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
+    ];
+
+    function getAll(){
+        return pokemonList;
+    }
+
+    function add(pokemon){
+        return pokemonList.push(pokemon);
+
+    }
+
+    return {
+        getAll,
+        add
+         };
+})();
 
 // Loops through the array of pokemon and points out which ones height is <= 70
-for (i = 0; i < pokemonList.length; i++) {
-    if (pokemonList[i].height <= 70) {
-        document.write(`<div>${pokemonList[i].name}: 
-        Height: ${pokemonList[i].height} / 
-        Types: ${pokemonList[i].types} - Tiny pokemon </div>`);
+pokemonList.forEach(function (element) {
+    if (element.height <= 70) {
+        document.write(`<div>${element.name}: 
+        Height: ${element.height} / 
+        Types: ${element.types} - Tiny pokemon </div>`);
 
     } else {
-        document.write(`<div>${pokemonList[i].name}: 
-        Height: ${pokemonList[i].height} / 
-        Types: ${pokemonList[i].types} </div>`);
+        document.write(`<div>${element.name}: 
+        Height: ${element.height} / 
+        Types: ${element.types} </div>`);
     }
-}
+})
