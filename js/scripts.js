263add278bc305560e2a5091f80478dc2d6f516a
alilein1990 <alina.leinweber@gmx.de>
2023-10-12 16:17:44 +0200

    added conditions to function add()

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+//The array of pokemon in IIFE with return function add & getAll
 let pokemonRepository = (function () {
     let pokemonList = [
         { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
@@ -6,23 +7,46 @@ let pokemonRepository = (function () {
         { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
     ];
 
-    function getAll(){
+    function getAll() {
         return pokemonList;
     }
 
-    function add(pokemon){
-        return pokemonList.push(pokemon);
+    function add(pokemon) {
+        if (typeof pokemon === 'object') {
 
+            let keys = Object.keys(pokemon).map(function(toLower){
+                return toLower.toLowerCase();
+            })
+
+            let validKeys = keys.every(function(validk){
+                return Object.keys(pokemonList[0]).map(function(toLower){
+                    return toLower.toLowerCase();
+                }).includes(validk);
+            })
+            
+            if(validKeys){
+                return pokemonList.push(pokemon);
+            }
+            else{
+                document.write('Error: Wrong keys. The keys are: name, height, types.');
+            }
+        } else {
+            document.write('Please add an object as the pokemon');
+        }
     }
 
+    function filter(){
+
+    }
     return {
         getAll,
-        add
-         };
+        add,
+        filter
+    };
 })();
 
 // Loops through the array of pokemon and points out which ones height is <= 70
-pokemonList.forEach(function (element) {
+pokemonRepository.getAll().forEach(function (element) {
     if (element.height <= 70) {
         document.write(`<div>${element.name}: 
         Height: ${element.height} / 
