cbcbe72d6e6ef1619edaa8499a2ef655cf41be98
alilein1990 <alina.leinweber@gmx.de>
2023-10-15 17:47:18 +0200

    update add func. & add  loadList & loadDetails func

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 //The array of pokemon in IIFE with return function add & getAll
 let pokemonRepository = (function () {
-    let pokemonList = [
-        { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
-        { name: 'Butterfree', height: 110, types: ['bug', ' flying'] },
-        { name: 'Ariados', height: 110, types: ['bug', ' poison'] },
-        { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
-    ];
+    // let pokemonList = [
+    //     { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
+    //     { name: 'Butterfree', height: 110, types: ['bug', ' flying'] },
+    //     { name: 'Ariados', height: 110, types: ['bug', ' poison'] },
+    //     { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
+    // ];
+
+    let pokemonList = [];
+    let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
     //returns a list of all pokemon contained in array pokemonList
     function getAll() {
         return pokemonList;
@@ -13,28 +16,32 @@ let pokemonRepository = (function () {
     // allows to add new pokemon to the array if it meets the needed conditions
     function add(pokemon) {
         //makes sure the type is object
-        if (typeof pokemon === 'object') {
+        if (typeof pokemon === 'object' && 'name' in pokemon) {
+            pokemonList.push(pokemon);
+        }else{
+            console.log('Wrong naming of pokemon');
+        } 
 
             // makes sures keys from pokemon match the keys from pokemonList 
-            let keys = Object.keys(pokemon).map(function (toLower) {
-                return toLower.toLowerCase();
-            })
+        //     let keys = Object.keys(pokemon).map(function (toLower) {
+        //         return toLower.toLowerCase();
+        //     })
 
-            let validKeys = keys.every(function (validk) {
-                return Object.keys(pokemonList[0]).map(function (toLower) {
-                    return toLower.toLowerCase();
-                }).includes(validk);
-            })
+        //     let validKeys = keys.every(function (validk) {
+        //         return Object.keys(pokemonList[0]).map(function (toLower) {
+        //             return toLower.toLowerCase();
+        //         }).includes(validk);
+        //     })
 
-            if (validKeys) {
-                return pokemonList.push(pokemon);
-            }
-            else {
-                console.log('Error: Wrong keys. The keys are: name, height, types.');
-            }
-        } else {
-            console.log('Please add an object as the pokemon.');
-        }
+        //     if (validKeys) {
+        //         return pokemonList.push(pokemon);
+        //     }
+        //     else {
+        //         console.log('Error: Wrong keys. The keys are: name, height, types.');
+        //     }
+        // } else {
+        //     console.log('Please add an object as the pokemon.');
+        // }
     }
     //allows to find pokemon by name
     function filter(name) {
@@ -74,27 +81,67 @@ let pokemonRepository = (function () {
     }
 
     // will show details of pokemon when function called through click
-    function showDetails(pokemon) {
-        console.log(pokemon);
-    };
 
+    function loadList() {
+        return fetch(apiUrl).then(function (response) {
+            return response.json();
+        }).then(function (json) {
+            json.results.forEach(function (item) {
+                let pokemon = {
+                    name: item.name,
+                    detailsUrl: item.url
+                };
+                add(pokemon);
+            });
+        }).catch(function (e) {
+            console.error(e);
+        })
+    }
+
+    function loadDetails(item) {
+        let url = item.detailsUrl;
+        return fetch(url).then(function (response) {
+            return response.json();
+        }).then(function (details) {
+            // Now we add the details to the item
+            item.imageUrl = details.sprites.front_default;
+            item.height = details.height;
+            item.types = details.types;
+        }).catch(function (e) {
+            console.error(e);
+        });
+    }
+    function showDetails(pokemon) {
+        loadDetails(pokemon).then(function () {
+            console.log(pokemon);
+        });
+    };
 
     return {
         getAll,
         add,
         filter,
         addListItem,
-        showDetails
+        showDetails,
+        loadList,
+        loadDetails
     };
 })();
 
 
-// pokemonRepository.add(vv); -> why i don’t get the error message i predefined?
+pokemonRepository.add({name:'Test', height: 60, types: ['fire']});
+
+pokemonRepository.loadList().then(function () {
+    // Now the data is loaded!
+    pokemonRepository.getAll().forEach(function (pokemon) {
+        pokemonRepository.addListItem(pokemon);
+    });
+});
 
 //call the function getAll from pokemonRepository, that returns the array
 //loop through array and perform on each object of the array the addListItem function
-pokemonRepository.getAll().forEach(function (pokemon) {
-    pokemonRepository.addListItem(pokemon);
+// pokemonRepository.getAll().forEach(function (pokemon) {
+//     pokemonRepository.addListItem(pokemon);
     // if (element.height <= 70) {
     //     document.write(`<div>${element.name}: 
     //     Height: ${element.height} / 
@@ -105,4 +152,4 @@ pokemonRepository.getAll().forEach(function (pokemon) {
     //     Height: ${element.height} / 
     //     Types: ${element.types} </div>`);
     // }
-})
+// })
