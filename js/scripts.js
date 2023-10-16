97173ef383232673e06a9046d372b4917254dc3e
alilein1990 <alina.leinweber@gmx.de>
2023-10-16 20:25:18 +0200

    add functoins & remove comments
    displayLoadingMessage & hideLoadingMessage functions been added and inserted to functions loadList & loadDetails

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,9 @@
 //The array of pokemon in IIFE with return function add & getAll
 let pokemonRepository = (function () {
-    // let pokemonList = [
-    //     { name: 'Bulbasaur', height: 70, types: ['grass', ' poison'] },
-    //     { name: 'Butterfree', height: 110, types: ['bug', ' flying'] },
-    //     { name: 'Ariados', height: 110, types: ['bug', ' poison'] },
-    //     { name: 'Slowbro', height: 160, types: ['psychic', ' water'] }
-    // ];
 
     let pokemonList = [];
     let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=150';
+
     //returns a list of all pokemon contained in array pokemonList
     function getAll() {
         return pokemonList;
@@ -16,56 +11,19 @@ let pokemonRepository = (function () {
     // allows to add new pokemon to the array if it meets the needed conditions
     function add(pokemon) {
         //makes sure the type is object
-        if (typeof pokemon === 'object' && 'name' in pokemon) {
+        if (typeof pokemon === 'object' && 'name' in pokemon && 'detailsUrl' in pokemon) {
             pokemonList.push(pokemon);
-        }else{
-            console.log('Wrong naming of pokemon');
-        } 
-
-            // makes sures keys from pokemon match the keys from pokemonList 
-        //     let keys = Object.keys(pokemon).map(function (toLower) {
-        //         return toLower.toLowerCase();
-        //     })
-
-        //     let validKeys = keys.every(function (validk) {
-        //         return Object.keys(pokemonList[0]).map(function (toLower) {
-        //             return toLower.toLowerCase();
-        //         }).includes(validk);
-        //     })
-
-        //     if (validKeys) {
-        //         return pokemonList.push(pokemon);
-        //     }
-        //     else {
-        //         console.log('Error: Wrong keys. The keys are: name, height, types.');
-        //     }
-        // } else {
-        //     console.log('Please add an object as the pokemon.');
-        // }
-    }
-    //allows to find pokemon by name
-    function filter(name) {
-        if (typeof name === 'string' && name !== '') {
-            let pokemonNames = pokemonList.map(function (pokemon) {
-                return pokemon.name.toLowerCase();
-            })
-            let lowerCaseName = name.toLowerCase();
-            nameInList = pokemonNames.includes(lowerCaseName);
-            if (nameInList) {
-                console.log('The pokemon: ' + name + ' is in the list! :D');
-            } else {
-                console.log('This pokemon doesn\'t exist in the list')
-            }
         } else {
-            console.log('Input must be a string.')
+            console.log('Wrong naming for pokemon');
         }
     }
+
     //display one single pokemon
     function addListItem(pokemon) {
         let pokemonAll = document.querySelector('.pokemon-list');
         let pokemonItem = document.createElement('li');
-
         let pokemonButton = document.createElement('button');
+
         pokemonButton.innerText = pokemon.name;
         pokemonButton.classList.add('button-class');
         pokemonItem.appendChild(pokemonButton);
@@ -80,9 +38,9 @@ let pokemonRepository = (function () {
         })
     }
 
-    // will show details of pokemon when function called through click
-
+    // will load only the name & url of the pokemon from the API
     function loadList() {
+        displayLoadingMessage();
         return fetch(apiUrl).then(function (response) {
             return response.json();
         }).then(function (json) {
@@ -93,43 +51,80 @@ let pokemonRepository = (function () {
                 };
                 add(pokemon);
             });
+            hideLoadingMessage();
         }).catch(function (e) {
             console.error(e);
+            hideLoadingMessage();
         })
     }
-
+    // load only image, height, type per pokemon, not the other details from the API
     function loadDetails(item) {
+        //deatilsUrl comes from loadList() - is the item.url
+        displayLoadingMessage();
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
             return response.json();
         }).then(function (details) {
             // Now we add the details to the item
+            //spirites & front_default defined in the API itselfs, as url was 
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
             item.types = details.types;
+            hideLoadingMessage();
         }).catch(function (e) {
             console.error(e);
+            hideLoadingMessage();
         });
     }
+
+    // will show details of pokemon when function called through click
     function showDetails(pokemon) {
         loadDetails(pokemon).then(function () {
             console.log(pokemon);
         });
     };
+    // TO DO code filter funtion
+    function searchPokemonByName(name) {
+        name = name.toLowerCase();
 
+        let foundPokemon = pokemonList.find(function (pokemon) {
+            return pokemon.name.toLowerCase() === name;
+        });
+
+        if (foundPokemon) {
+            console.log('Found:', foundPokemon);
+        } else {
+            console.log('Pokemon not found');
+        }
+    }
+
+    function displayLoadingMessage() {
+        let messageElement = document.createElement('div');
+        messageElement.classList.add('load-message');
+        messageElement.innerHTML = 'The pokemon are loading...';
+        document.body.appendChild(messageElement);
+        return messageElement;
+    }
+
+    function hideLoadingMessage() {
+        let messageElement = document.querySelector('.load-message');
+        document.body.removeChild(messageElement);
+    }
     return {
         getAll,
         add,
-        filter,
         addListItem,
         showDetails,
         loadList,
-        loadDetails
+        loadDetails,
+        searchPokemonByName
     };
 })();
 
 
-pokemonRepository.add({name:'Test', height: 60, types: ['fire']});
+// pokemonRepository.add({name:'Test', detailsUrl:'https://pokeapi.co/api/v2/pokemon/1/'})
+// pokemonRepository.searchPokemonByName('bulbasaur');    ---> doesnt work :(
+//console.log(pokemonRepository.getAll());   ---> doesnt work :(
 
 pokemonRepository.loadList().then(function () {
     // Now the data is loaded!
@@ -137,19 +132,3 @@ pokemonRepository.loadList().then(function () {
         pokemonRepository.addListItem(pokemon);
     });
 });
-
-//call the function getAll from pokemonRepository, that returns the array
-//loop through array and perform on each object of the array the addListItem function
-// pokemonRepository.getAll().forEach(function (pokemon) {
-//     pokemonRepository.addListItem(pokemon);
-    // if (element.height <= 70) {
-    //     document.write(`<div>${element.name}: 
-    //     Height: ${element.height} / 
-    //     Types: ${element.types} - Tiny pokemon </div>`);
-
-    // } else {
-    //     document.write(`<div>${element.name}: 
-    //     Height: ${element.height} / 
-    //     Types: ${element.types} </div>`);
-    // }
-// })
